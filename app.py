d215889a8722572260b71f544ac87bce9728585f
Rodrigo Franco <49168245+franc703@users.noreply.github.com>
2023-05-29 04:37:36 +0000

    Debug app file

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from transformers import pipeline
 import gradio as gr
 
-
-model = pipeline(
-    "summarization",
-)
+model = pipeline("summarization")
 
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-
 # create an interface for the model
-with gr.Blocks() as demo:
-    textbook = gr.Textbook(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn=predict, inputs=textbook, outputs="text")
+iface = gr.Interface(fn=predict, inputs=gr.inputs.Textbox(lines=4, placeholder="Enter text block to summarize"), outputs="text")
 
-demo.launch()
+iface.launch()
